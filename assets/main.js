3c5a5a9563fe7f7ff12004fb000b4f113ff756a2
vera <verapoles1@mail.ru>
2019-12-13 15:08:10 +0300

    project

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function() {
+    $("#owl1").owlCarousel({
+   	items: 1,
+   	loop: true,
+    autoplay: true,
+   	dots: false, 
+    smartSpeed: 2000,
+    autoplayTimeout: 3000
+   });	
+
+   $(".main__block-bars").fancybox();
+   $(".main__block-search").fancybox();
+   $(".section__hidden-bars").fancybox();
+   $(".section__hidden-search").fancybox();
+
+   $(".top").on("click", function(e) {
+            e.preventDefault();
+            $("html, body").animate({scrollTop: 0}, 500);
+   });
+
+});
